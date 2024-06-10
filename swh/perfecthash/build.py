c15c6ecc3b6e95735e2e1afae0dacddef9fa0ad9
Antoine Lambert <anlambert@softwareheritage.org>
2024-06-10 15:02:58 +0200

    build.py: Fix extension build on macOS by finding libcmph installation
    On macOS, libcmph can be easily installed using Homebrew or MacPorts so ensure
    the Python extension can be built on that platform by checking libcmph paths
    and updating compilation flags accordingly.

--- swh/perfecthash/build.py
+++ swh/perfecthash/build.py
@@ -1,9 +1,10 @@
-# Copyright (C) 2021-2022  The Software Heritage developers
+# Copyright (C) 2021-2024  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-import pathlib
+from pathlib import Path
+import platform
 
 from cffi import FFI
 
@@ -39,10 +40,19 @@ extern const int shard_key_len;
 
 library_dirs = []
 extra_compile_args = ["-D_FILE_OFFSET_BITS=64"]
-bundled_cmph = pathlib.Path(__file__).parent.parent.parent / "cmph"
+bundled_cmph = Path(__file__).parent.parent.parent / "cmph"
 if bundled_cmph.is_dir():
     library_dirs.append(str(bundled_cmph / "lib"))
     extra_compile_args.append(f"-I{bundled_cmph}/include")
+elif platform.system() == "Darwin" and Path("/usr/local/include/cmph.h").is_file():
+    # ensure to find cmph on macOS if installed with Homebrew using "brew install libcmph"
+    library_dirs.append("/usr/local/lib")
+    extra_compile_args.append("-I/usr/local/include")
+elif platform.system() == "Darwin" and Path("/opt/local/include/cmph.h").is_file():
+    # ensure to find cmph on macOS if installed with MacPorts using "port install cmph"
+    library_dirs.append("/opt/local/lib")
+    extra_compile_args.append("-I/opt/local/include")
+
 
 ffibuilder.set_source(
     "swh.perfecthash._hash_cffi",
